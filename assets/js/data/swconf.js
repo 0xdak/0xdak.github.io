695f44de1389bdd47f8881d3413ced1d05482dd9
yuznumara <58743788+0xdak@users.noreply.github.com>
2025-07-20 17:01:53 +0300

    initial commit

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,26 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1753019868',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/portfolio/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
